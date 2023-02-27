3eaea1efd22d7609292f4e18e023cb3287cfe351
Haidar Alkhazaly <62010519+heider110@users.noreply.github.com>
2023-02-27 23:34:44 +0100

    troublehooting

--- models/passport.js
+++ models/passport.js
@@ -29,7 +29,7 @@ passport.use(new LocalStrategy(User.authenticate()));
 passport.use(new GoogleStrategy({
     clientID: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/auth/google/secrets",
+    callbackURL: "https://crypto-tracker110.herokuapp.com/auth/google/secrets",//http://localhost:3000/auth/google/secrets
     userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
   },
   function(accessToken, refreshToken, profile, done) {
